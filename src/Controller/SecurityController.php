ed46104f8288b40fe0dcd760f9d9703ab29c3308
jocelyn <grelierjocelyn@gmail.com>
2024-01-31 11:03:58 +0100

    User-signup-login-logout

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+
+class SecurityController extends AbstractController
+{
+
+private EntityManagerInterface $em;
+
+    public function __construct(private $formLoginAuthenticator,EntityManagerInterface $em)
+    {  
+        $this->em = $em;
+    }
+
+    #[Route('/signup', name: 'signup')]
+    public function signup(Request $request,
+    UserPasswordHasherInterface $passwordHasher,
+    UserAuthenticatorInterface $userAuthenticator
+    ): Response
+    {
+        $user = new User();
+        $userForm = $this->createForm(UserType::class, $user);
+        $userForm->handleRequest($request);
+
+        if($userForm->isSubmitted() && $userForm->isValid()) {
+
+            $hash = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+            $this->em->persist($user);
+            $this->em->flush();
+
+                // Connexion de l'utilisateur aprés inscription
+            return $userAuthenticator->authenticateUser($user, $this->formLoginAuthenticator, $request);
+
+        }
+
+        return $this->render('security/signup.html.twig', [
+            'form' => $userForm->createView()
+        ]);
+    }
+
+     
+    #[Route('/login', name: 'login')]
+    public function login(
+        AuthenticationUtils $authenticationUtils
+    ): Response
+    {
+         // erreur dauthentification 
+         $error = $authenticationUtils->getLastAuthenticationError();
+         // récupère le nom entré par l'utilisateur lors de la derniére connxion
+         $username = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'error' => $error,
+            'username' => $username
+        ]);
+    }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout()
+    {  
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: 'email', message: "Cet email est déjà utilisé.")]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    #[Assert\NotBlank(message: "Veuillez renseigner une adresse mail.")]
+    #[Assert\Email(message: "Veuillez renseigner une adresse mail valide.")]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    #[Assert\Regex(
+        pattern: '/^[a-zA-Z0-9!@#$%^&*(),.?:{}|]*$/',
+        message: 'Le champ peut contenir des lettres majuscules, minuscules, chiffres et certains symboles.'
+    )] 
+    #[Assert\NotBlank(message: "Veuillez renseigner un mot de passe.")]
+    #[Assert\Length(min: 5, minMessage:" Le mot de passe doit faire plus de 5 caracteres.")]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Veuillez renseigner votre prénom.")]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Veuillez renseigner votr nom.")]
+    private ?string $lastname = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function __construct()
+    {
+        $this->roles = ['ROLE_USER'];
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getFullname() : string {
+        return $this->firstname . ' ' .$this->lastname;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
